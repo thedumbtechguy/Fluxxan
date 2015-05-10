568139537f41e6c8d05b915ea0e9c82cbd4d6f69
frostymarvelous <sfroelich01@gmail.com>
2015-05-10 14:30:12 +0000

    added Exception listener to enable dispatch to be async

---
+++ droidfluxlib/src/main/java/com/whisppa/droidfluxlib/DispatchExceptionListener.java
@@ -0,0 +1,8 @@
+package com.whisppa.droidfluxlib;
+
+/**
+ * Created by user on 5/10/2015.
+ */
+public interface DispatchExceptionListener {
+    public void onException(Exception e);
+}
--- droidfluxlib/src/main/java/com/whisppa/droidfluxlib/Dispatcher.java
+++ droidfluxlib/src/main/java/com/whisppa/droidfluxlib/Dispatcher.java
@@ -10,6 +10,7 @@ import java.util.Set;
 public interface Dispatcher {
     public void addStore(@NonNull String name, @NonNull Store store);
     public void dispatch(@NonNull Payload payload) throws Exception;
+    public void dispatch(@NonNull Payload payload, DispatchExceptionListener dispatchExceptionListener) throws Exception;
     public void waitFor(String waitingStoreName, Set<String> storeNames, Callback callback) throws Exception;
     public boolean isDispatching();
 }
